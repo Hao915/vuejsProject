1a424696b35adfd94eb76c4276ab66a27f742ecb
Hao <lihao.915@gmail.com>
2018-06-17 21:25:24 +1000

    Source selection component finish

--- src/components/SourceSelection.vue
+++ src/components/SourceSelection.vue
@@ -1,12 +1,44 @@
 <template>
     <div class="sourceselection">
-        SourceSelection
+        <div class="jumbotron">
+       <h2><span class="glyphicon glyphicon-list-alt"></span> News List</h2>
+
+        <h4>Select News source</h4>
+        <select class="form-control" v-on:change="sourceChanged">
+            <option v-bind:value="source.id" v-for="source in sources">{{source.name}}</option>
+        </select>
+        <div v-if="source">
+            <h6>{{source.description}}</h6>
+            <a v-bind:href="source.url" class="btn btn-primary" target="_blank">Go To {{source.name}} Website</a>
+        </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'sourceselection'
+    name: 'sourceselection',
+    data () {
+     return{
+        sources: [],
+        source: '' 
+     }
+    },
+    methods: {
+        sourceChanged: function (e) {
+            for (let i=0;i<this.sources.length;i++) {
+                if (this.sources[i].id == e.target.value) {
+                    this.source = this.sources[i];
+                }
+            }
+        }
+    },
+    created: function () {
+        this.$http.get('https://newsapi.org/v1/sources?language=en')
+        .then(response => {
+            this.sources = response.data.sources;
+        });
+    }
 }
 </script>
 
